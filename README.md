42d41c41eae4751ae7ffa017582878766e536147
NathanGrenier <nathangrenier01@gmail.com>
2025-02-24 03:31:57 -0500

    fix: tailwind css issues

--- README.md
+++ README.md
@@ -7,3 +7,13 @@
 | Name           | Student ID | Email                     |
 | -------------- | ---------- | ------------------------- |
 | Nathan Grenier | 40250986   | nathangrenier01@gmail.com |
+
+# Contributing
+
+## Icon Library
+
+Shadcn recommend using [Lucide Icons](https://lucide.dev/icons/). The package is already installed, just import the icon you want to use.
+
+## Importing Shadcn Components
+
+Make sure to use `npx shadcn@canary add {component}` to import the component you want.
--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,53 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+import { ModeToggle } from "@/components/mode-toggle";
+import reactLogo from "@/assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex flex-col items-center justify-center min-h-screen gap-4">
+        <div className="flex gap-8 items-center mb-4">
+          <img src={viteLogo} className="h-28 w-28" alt="Vite logo" />
+          <img
+            src={reactLogo}
+            className="animate-spin h-28 w-28"
+            alt="React logo"
+          />
+        </div>
+        <h1 className="font-bold">Vite + React!</h1>
+        <div className="flex flex-col items-center justify-center space-y-4">
+          <div className="flex gap-4">
+            <Button
+              variant="default"
+              size="lg"
+              onClick={() => setCount((count) => count + 1)}
+            >
+              count is {count}
+            </Button>
+            <Button
+              variant="outline"
+              onClick={() => {
+                const currentTime = new Date().toLocaleTimeString();
+                toast.success("Event has been created!", {
+                  description: `Created at: ${currentTime}`,
+                });
+              }}
+            >
+              Toast!
+            </Button>
+            <ModeToggle />
+          </div>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
       </div>
-      <h1 className="font">Vite + React!</h1>
-      <div className="card">
-        <Button
-          variant="outline"
-          size="lg"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
