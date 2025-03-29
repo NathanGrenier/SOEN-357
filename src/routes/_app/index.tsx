30d3f363fc9aa6f68aacd1a6f59dd827a5357865
Nirav Patel <niravp0703@gmail.com>
2025-03-29 15:02:10 -0400

    fix: responsiveness

--- src/routes/_app/index.tsx
+++ src/routes/_app/index.tsx
@@ -89,8 +89,8 @@ function Index() {
           <div className="container mx-auto max-w-6xl px-4 md:px-6">
             <div className="grid gap-6 lg:grid-cols-2 lg:gap-12 xl:grid-cols-2">
               <div className="flex flex-col justify-center space-y-4">
-                <div className="space-y-2">
-                  <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
+                <div className="space-y-2 md:mx-auto md:block">
+                  <h1 className="text-2xl font-bold tracking-tighter md:text-4xl">
                     Step Into Style at Sneaker Hub
                   </h1>
                   <p className="text-muted-foreground max-w-[600px] md:text-xl">
@@ -98,7 +98,7 @@ function Index() {
                     limited editions to timeless classics.
                   </p>
                 </div>
-                <div className="mx-auto flex space-x-4 min-[400px]:flex-row md:w-full">
+                <div className="mx-auto space-x-4 min-[400px]:flex-row lg:flex lg:w-full">
                   <Link
                     href="/footwear?page=1&query=&category=All Categories"
                     to={"/"}
@@ -286,17 +286,17 @@ function Index() {
       </main>
 
       <footer className="mx-auto w-full max-w-6xl py-12 md:py-24 lg:py-32">
-        <div className="container px-4 md:px-6">
-          <div className="grid gap-8 sm:grid-cols-2 md:grid-cols-4">
-            <div className="space-y-4">
+        <div className="container mx-auto px-4 md:px-6">
+          <div className="ml-4 grid grid-cols-2 gap-8 md:grid-cols-4">
+            <div className="flex flex-col items-center justify-center space-y-4 md:mr-4 md:border-r md:pr-6">
               <Link href="/" className="flex items-center gap-2" to={"/"}>
                 <SiteLogo size="sm" />
                 <span className="text-xl font-bold">Sneaker Hub</span>
               </Link>
-              <p className="text-muted-foreground text-sm">
+              <p className="text-muted-foreground text-center text-sm">
                 Your destination for premium sneakers and streetwear.
               </p>
-              <div className="flex gap-4">
+              <div className="flex justify-center gap-4">
                 <Link
                   href="#"
                   className="text-muted-foreground hover:text-foreground"
